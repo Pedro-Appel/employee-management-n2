8624c12cc7320350c40eadda4db638699ca6ad40
unknown <appelpedroappel@gmail.com>
2022-11-18 22:12:39 -0300

    feat: dont even know

--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,39 +1,51 @@
 import './styles/style.css';
 import NavigationButton from './components/NavigationButton';
 import { useNavigate } from "react-router-dom";
+import useFetch from 'use-http'
+
 
 const baseURL = `https://mack-webmobile.vercel.app/api/users`;
 
 export function CreateUser(){
+
   const navigate = useNavigate()
-  function createEmployee(event){
+  const opt = {
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    redirect: 'follow',
+    referrerPolicy: 'no-referrer'
+  }
+  const { post, response  } = useFetch(baseURL)
+
+  const newUser = async (event) => {
     const formData = new FormData(event.target);
-    const json = JSON.stringify(Object.fromEntries(formData.entries()));
-
-    fetch(baseURL, {
-      method: 'POST',
-      mode: 'cors',
-      cache: 'no-cache',
-      credentials: 'same-origin',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: json 
-    })
-    .then(setTimeout(function() {
-      navigate('/')
-    }, 1000))
-
+    const json = Object.fromEntries(formData.entries());
     event.preventDefault();
+    await post("", {
+      avatar: json.avatar,
+      date: json.date,
+      email: json.email,
+      name: json.name,
+      salary: json.salary,
+      status: json.status,
+    });
+    if (response.ok) {
+      setTimeout(function() {
+        navigate('/')
+      }, 1000)
+    };
   }
 
   return (
     <div className="page_create_user">
       <h1 className="page_title">Criar um Funcionario</h1>
         <NavigationButton routeToNavigate='/' name='Buscar Funcionarios'/>
-      <form onSubmit={createEmployee}>
+      <form onSubmit={newUser}>
         
         <div><label>Name:<input type="text" name="name" /></label></div>
         <div><label>Email: <input type="text" name="email" /></label></div>
--- src/ListUsers.js
+++ src/ListUsers.js
@@ -1,30 +1,75 @@
 import "./styles/style.css";
 import "./styles/employeeTable.css";
-import { useState, useEffect } from "react";
+import { useCallback, useState, useEffect} from "react";
 import NavigationButton from "./components/NavigationButton";
+import useFetch from 'use-http'
 
 const baseURL = `https://mack-webmobile.vercel.app/api/users`;
 
 export function ListUser() {
+
   const [employees, setEmployees] = useState([]);
+  const options = {};
+  const { 
+    get, 
+    response, 
+    loading, 
+    error, 
+    data = [] 
+  } = useFetch(`https://mack-webmobile.vercel.app/api/users`, options, [])
 
-  function buscarUsuarios() {
-    fetch(baseURL)
-      .then((response) => response.json())
-      .then((data) => setEmployees(data));
+  function retrieveEmployees(){
+    console.log("retrieving ...")
+    fetch(baseURL).then((res)=> res.json())
+    .then((res) => setEmployees(res))
+  }
+  function removeEmployees(id){
+    fetch(baseURL+`/${id}`, {method: 'DELETE'})
+    .then((res)=> res.json()).then(removeEmployees())
   }
 
-  function deleteUser(id) {
-    fetch(baseURL + `/${id}`, { method: "DELETE" })
-      .then((res) => console.log(res))
-      .then(() => buscarUsuarios());
-  }
+  useEffect(()=>{
+    retrieveEmployees()
+  },[])
 
-  useEffect(() => {
-    buscarUsuarios();
-  }, []);
+  // useEffect(() => {
+  //   // console.log("First Fetch", data)
+  //   if(loading){
+  //     console.log("loading...")
+  //   }
+  //   if(error){
+  //     console.log("error...")
+  //   }
+  //   if(data !== [] && employees === []){
+  //     setEmployees(data)
+      
+  //   }
+  //   else{
+  //     loadEmployees()
+
+  //   }
+  // }, [error, loading, data, employees]) 
+
+
+  // async function loadEmployees(){
+  //   const newEmployees = await get()
+  //   console.log("Dentro da busca ",newEmployees)
+    
+  //   setTimeout(function() {
+  //     setEmployees(newEmployees)
+  //   }, 500)
+  // }
+  // async function removeEmployee(e) {
+  //   fetch(baseURL+`/${e}`, {method: 'DELETE'})
+    
+  //   setTimeout(function() {
+  //     setEmployees([])
+  //   }, 500)
+  //   }
 
   return (
+    <>
+    
     <div className="page">
       <h1 className="page-title">LISTA DE FUNCIONARIOS</h1>
 
@@ -37,47 +82,51 @@ export function ListUser() {
           />
         </div>
         <div className="page-button-container">
-          <button onClick={() => buscarUsuarios()}>Buscar Funcionarios</button>
+          <button onClick={retrieveEmployees}>Buscar Funcionarios</button>
         </div>
       </div>
-
-      <div className="page-tables">
-        <table className="page__table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Salary</th>
-              <th>Date</th>
-              <th>Status</th>
-              <th>See Details</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          {employees.map((e) => {
-            return (
-              <tbody>
-                <tr>
-                  <td>{e.name}</td>
-                  <td>{e.email}</td>
-                  <td>{e.salary}</td>
-                  <td>{e.date}</td>
-                  <td>{e.status}</td>
-                  <td>
-                    <NavigationButton
-                      routeToNavigate={`/get/${e._id}`}
-                      name="Ver Detalhes"
-                    />
-                  </td>
-                  <td>
-                    <button onClick={() => deleteUser(e._id)}></button>
-                  </td>
-                </tr>
-              </tbody>
-            );
-          })}
-        </table>
+      {error && <h1>Error!</h1>}
+      {loading && <h1>Loading...</h1>}
+      {
+        <div className="page-tables">
+          <table className="page__table">
+            <thead>
+              <tr >
+                <th>Name</th>
+                <th>Email</th>
+                <th>Salary</th>
+                <th>Date</th>
+                <th>Status</th>
+                <th>See Details</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            {employees.map((e) => {
+              return (
+                <tbody>
+                  <tr>
+                    <td>{e.name}</td>
+                    <td>{e.email}</td>
+                    <td>{e.salary}</td>
+                    <td>{e.date}</td>
+                    <td>{e.status}</td>
+                    <td>
+                      <NavigationButton
+                        routeToNavigate={`/get/${e._id}`}
+                        name="Ver Detalhes"
+                      />
+                    </td>
+                    <td>
+                      <button onClick={() => removeEmployees(e._id)}></button>
+                    </td>
+                  </tr>
+                </tbody>
+              );
+            })}
+          </table>
+        </div>
+    }
       </div>
-    </div>
+    </>
   );
 }
