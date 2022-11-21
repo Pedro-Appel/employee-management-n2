17f2f3409f38d2df884c732af6c109ffcd778a88
lucca <luccaraposo80@gmail.com>
2022-11-21 11:57:39 -0300

    filter feature

--- src/ListUsers.js
+++ src/ListUsers.js
@@ -1,46 +1,65 @@
 import "./styles/style.css";
 import "./styles/employeeTable.css";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import NavigationButton from "./components/NavigationButton";
-import useFetch from 'use-http'
+import useFetch from "use-http";
 
 const baseURL = `https://mack-webmobile.vercel.app/api/users`;
 
 export function ListUser() {
   const [employees, setEmployees] = useState([]);
 
-  const options = {cache: 'no-cache'};
-  const { 
-    loading, 
-    error, 
-  } = useFetch(`https://mack-webmobile.vercel.app/api/users`, options)
+  const options = { cache: "no-cache" };
+  const { loading, error } = useFetch(
+    `https://mack-webmobile.vercel.app/api/users`,
+    options
+  );
 
-  async function retrieveEmployees(){
+  async function retrieveEmployees() {
     fetch(baseURL, options)
-    .then((res)=> res.json())
-    .then((res)=> setEmployees(res))
+      .then((res) => res.json())
+      .then((res) => setEmployees(res));
   }
 
-  function removeEmployees(id){
-    fetch(baseURL+`/${id}`, {method: 'DELETE'})
-    .then(setTimeout(function() {
-      retrieveEmployees()
-    }, 500))
+  function removeEmployees(id) {
+    fetch(baseURL + `/${id}`, { method: "DELETE" }).then(
+      setTimeout(function () {
+        retrieveEmployees();
+      }, 500)
+    );
   }
 
-  useEffect(() => {  
-  
-  async function loadInitial() {
-    retrieveEmployees()
+  function selectFilter() {
+    var select = document.getElementById("filter-select");
+    var value = select.options[select.selectedIndex].value;
+    if (value == "Active") {
+      filterEmployeesByStatus(value);
+      console.log(employees)
+    } 
+    if (value == "Inactive") {
+      filterEmployeesByStatus(value);
+    }
+    console.log(employees)
   }
-  
-  // async function loadInitial() {
-  //   await get('')
-  //   var e = await response.json()
-  //   if (response.ok) setEmployees(e)
-  // }
 
-  loadInitial() }, []) // componentDidMount
+  function filterEmployeesByStatus(status) {
+    var result = employees.filter((employee) => employee.status == status);
+    setEmployees(result);
+  }
+
+  useEffect(() => {
+    async function loadInitial() {
+      retrieveEmployees();
+    }
+
+    // async function loadInitial() {
+    //   await get('')
+    //   var e = await response.json()
+    //   if (response.ok) setEmployees(e)
+    // }
+
+    loadInitial();
+  }, []); // componentDidMount
 
   return (
     <>
@@ -52,7 +71,7 @@ export function ListUser() {
               class="option-button"
               routeToNavigate="/create"
               name="Adicionar Funcionário"
-              />
+            />
           </div>
           <div className="page-button-container">
             <button className="navigate-button" onClick={retrieveEmployees}>
@@ -60,6 +79,22 @@ export function ListUser() {
             </button>
           </div>
         </div>
+        <div className="filter-container">
+          <div className="filter">
+            <div id="filter-title" className="input-title">
+              Filtrar por:
+            </div>
+            <select id="filter-select" className="w3-select" type="text" name="status">
+              <option value="" disabled selected></option>
+              <option value="Active">Active</option>
+              <option value="Inactive">Inactive</option>
+            </select>
+            <button className="option-button" onClick={() => retrieveEmployees().then(selectFilter())}>
+              Filtrar
+            </button>
+          </div>
+        </div>
+        <br></br>
         <div className="page-tables">
           <table className="page__table">
             <thead>
@@ -73,40 +108,45 @@ export function ListUser() {
                 <th>Delete</th>
               </tr>
             </thead>
-            {loading && <div> <h1> Loading....</h1> </div>}
+            {loading && (
+              <div>
+                {" "}
+                <h1> Loading....</h1>{" "}
+              </div>
+            )}
             {error && <p>ERROR... </p>}
-            {employees && 
-            employees.map((e) => {
-              return (
-                <tbody>
-                  <tr>
-                    <td>{e.name}</td>
-                    <td>{e.email}</td>
-                    <td>{e.salary}</td>
-                    <td>{e.date}</td>
-                    <td>{e.status}</td>
-                    <td>
-                      <NavigationButton
-                        class="navigate-button"
-                        routeToNavigate={`/get/${e._id}`}
-                        name="Editar"
+            {employees &&
+              employees.map((e) => {
+                return (
+                  <tbody>
+                    <tr>
+                      <td>{e.name}</td>
+                      <td>{e.email}</td>
+                      <td>{e.salary}</td>
+                      <td>{e.date}</td>
+                      <td>{e.status}</td>
+                      <td>
+                        <NavigationButton
+                          class="navigate-button"
+                          routeToNavigate={`/get/${e._id}`}
+                          name="Editar"
                         />
-                    </td>
-                    <td>
-                      <button className="delete-button" onClick={()=>removeEmployees(e._id)}>
-                        X
-                      </button>
-                    </td>
-                  </tr>
-                </tbody>
-              );
-            })}
+                      </td>
+                      <td>
+                        <button
+                          className="delete-button"
+                          onClick={() => removeEmployees(e._id)}
+                        >
+                          X
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                );
+              })}
           </table>
         </div>
-
-        
-
-    </div>
-  </>
+      </div>
+    </>
   );
 }
